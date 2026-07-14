ff675e79bb088946e2154812b4bb9da5b8adedce
ksen010 <kousik.sen@pwc.com>
2026-07-14 15:25:55 +0530

    update code

--- HealthInsuranceClaimAPI/HealthInsuranceClaimAPI/Data/HealthInsuranceContext.cs
+++ HealthInsuranceClaimAPI/HealthInsuranceClaimAPI/Data/HealthInsuranceContext.cs
@@ -108,7 +108,7 @@ namespace HealthInsuranceClaimAPI.Data
                 {
                     Id = 1,
                     Email = "kousik.sen@gmail.com",
-                    PasswordHash = "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9", 
+                    PasswordHash = "", 
                     Role = UserRole.Admin,
                     FirstName = "Admin",
                     LastName = "User",
